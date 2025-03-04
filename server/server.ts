30b8dd7bdd36bffa937c1dd0d0bcea65d3a6d15e
Efrain Palacios Mosquera <efpalaciosmo@unal.edu.co>
2025-03-03 20:51:45 -0500

    fixed port

--- server/server.ts
+++ server/server.ts
@@ -20,7 +20,7 @@ class Server {
 
     constructor() {
         this.app  = express();
-        this.port = process.env.PORT || '8000';
+        this.port = process.env.PORT || '8080';
 
         // Métodos iniciales
         this.dbConnection();
